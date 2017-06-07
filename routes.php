25f3322c0c0ff595efabc6065e58d4290082d866
Brandon Bronisz <bbronisz@thedmsgrp.com>
2017-06-07 15:42:13 -0400

    [MA-40] Fixed bug and refactored routes.php

--- routes.php
+++ routes.php
@@ -1,48 +1,4 @@
 <?php
 
-Route::post('/api/dynamicnumbers/numbers', function() {
-
-    $defaultNumber = \TheDMSGrp\DynamicPhones\Models\Settings::get('default_number', '8669841240');
-    $cookieLife = \TheDMSGrp\DynamicPhones\Models\Settings::get('cookie_life', 1);
-
-    if (empty($defaultNumber)) {
-        $defaultNumber = '8669841240';
-    }
-
-    $numbers = \TheDMSGrp\DynamicPhones\Models\Phone::select('phone', 'parameters', 'urls')->get()->toArray();
-
-    foreach ($numbers as &$number) {
-
-        // Convert parameters format
-        if (is_array($number['parameters'])) {
-
-            $parameters = [];
-
-            foreach ($number['parameters'] as $parameter) {
-                $parameters[$parameter['dropdown']] = $parameter['value'];
-            }
-
-            $number['parameters'] = $parameters;
-        }
-
-        // Convert urls format
-        if (is_array($number['urls'])) {
-
-            $urls = [];
-
-            foreach ($number['urls'] as $url) {
-                array_push($urls, $url['urls']);
-            }
-
-            $number['urls'] = $urls;
-
-        }
-    }
-
-    return [
-        'default' => $defaultNumber,
-        'phones' => $numbers,
-        'cookieExpiration' => $cookieLife
-    ];
-});
+Route::post('/api/dynamicnumbers/numbers', 'TheDMSGrp\DynamicPhones\Controllers\ApiController@fetchNumbers');
 
